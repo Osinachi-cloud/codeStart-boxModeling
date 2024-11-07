b2a0a7286ba79a8b194628457cf554fde21ae4cb
user <user@MacBook-Pro.local>
2024-11-07 08:29:00 +0100

    operators

--- app.js
+++ app.js
@@ -35,60 +35,24 @@
 // console.log(typeof(car));
 
 
-// // Operators
+// Operators
 
-// const previousBalance = 2;
-// const deposit = 3;
-// console.log(previousBalance + deposit);
+const previousBalance = 2;
+const deposit = 3;
+console.log(previousBalance + deposit);
 
-// const currentBalance = 5;
-// const withdrawal = 2;
+const currentBalance = 5;
+const withdrawal = 2;
 
-// console.log(currentBalance - withdrawal);
+console.log(currentBalance - withdrawal);
 
-// console.log(10 / 2);
-// console.log(4 * 3);
-// console.log(5 % 2);
-// const value = 45;
-// console.log(5 == 4);
-// console.log(5 === "5");
+console.log(10 / 2);
+console.log(4 * 3);
+console.log(5 % 2);
+const value = 45;
+console.log(5 == 4);
+console.log(5 === "5");
 
 
-// String methods
 
-let firstName = "John";
-let lastName = 'Doe';
-let middleName = `Smith`;
-let space = " ";
-
-console.log(firstName);
-console.log(lastName);
-console.log(middleName);
-
-conncartenation
-
-console.log(firstName + " " + lastName + " " + middleName);
-console.log(`${firstName} ${lastName} ${middleName}`);
-
-console.log(firstName.length);
-console.log(firstName[1]);
-console.log(firstName.charAt(2));
-
-console.log(firstName.toUpperCase());
-console.log(lastName.toLowerCase());
-
-console.log(firstName.startsWith("j"));
-console.log(firstName.endsWith("n"));
-
-console.log(firstName.includes("o"));
-
-console.log(firstName.indexOf("h"));
-
-let sentence = "I Love Yams";
-console.log(sentence.replace("Yams", "Plantains"));
-
-let sentenceWhiteSpace = " I Love Yams ";
-
-console.log(sentenceWhiteSpace);
-console.log(sentenceWhiteSpace.trim());
 
